c12f94975bc7ae4a1bcdf3dc0e707521a2eaaaf9
thejas <thejaskala308@gmail.com>
2023-01-28 22:42:09 +0530

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 app.get('/',(req,res)=>{
 	res.send("Hello world")
 })
-const {Client,GatewayIntentBits} = require('discord.js');
+const {Client,GatewayIntentBits,MessageAttachment} = require('discord.js');
 const client = new Client({intents:[
 	GatewayIntentBits.Guilds,
 	GatewayIntentBits.GuildMessages,
@@ -23,25 +23,40 @@ const openai = new OpenAIApi(configuration);
 
 client.on('messageCreate',async function (message,channelID){
 	try{
+		console.log(message.content)
+		if(message.content.toLowerCase().includes('#generate')){
+			// const response = await openai.createImage({
+			//   prompt: message.content,
+			//   n: 1,
+			//   size: "1024x1024",
+			// });
+			return ;
+		}
+
 		if(message.author.bot || !message.content.toLowerCase().includes('thejas') ) return;
 		
 		// message.channel.startTyping();
-		console.log(message.content)
-	
+		
+		
 		const response = await openai.createCompletion({
-		  model: "text-davinci-003",
-		  prompt: `Thejas is a chatbot that reluctantly answers questions with sarcastic responses:\n\n
-		  ${message.author.username}:${message.content}\n
-		  Thejas:
-		  `,
-		  temperature: 1,
-		  max_tokens: 200,
-		  top_p: 0.3,
-		  frequency_penalty: 0.5,
-		  presence_penalty: 0,
+		   model: "text-davinci-003",
+		   prompt: `Thejas is a chatbot that reluctantly answers questions with technical responses:\n\n
+		   ${message.author.username}:${message.content}\n
+		   Thejas:
+		   `,
+		   temperature: 1,
+		   max_tokens: 200,
+		   top_p: 0.3,
+		   frequency_penalty: 0.5,
+		   presence_penalty: 0,
 		});
-		// message.channel.stopTyping();
 		message.reply(response.data.choices[0].text)
+		
+		message.reply({content:`Your Prompt Image`,files:[response.data.data[0].url]});
+		
+		
+		
+		// message.channel.stopTyping();
 
 		
 	}catch(err){
