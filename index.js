2a2a485ae3cfa1144cc08b6d90752741e9fa43e0
thejas <thejaskala308@gmail.com>
2023-01-28 22:51:39 +0530

    make it better

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const client = new Client({intents:[
 
 const {Configuration,OpenAIApi} = require('openai');
 const configuration = new Configuration({
-	organization:process.env.OPENAI_ORG,
 	apiKey:process.env.OPENAI_KEY
 })
 
